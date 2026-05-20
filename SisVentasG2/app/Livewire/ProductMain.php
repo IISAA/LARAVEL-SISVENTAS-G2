0b2208ce85d0ed6425c7ee301ff1fbae19972bda
IISAA <armutoisaidd@gmail.com>
2026-05-20 11:15:24 -0500

    Segunda parte

--- SisVentasG2/app/Livewire/ProductMain.php
+++ SisVentasG2/app/Livewire/ProductMain.php
@@ -3,13 +3,61 @@
 namespace App\Livewire;
 
 use App\Models\Product;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class ProductMain extends Component
 {
+    public $search, $descripcion, $id;
+
+    #[validate('required')]
+    public $nombre, $cantidad, $precio, $disponible;
+
     public function render()
     {
-        $productos=Product::all();
-        return view('livewire.product-main',compact('productos'));
+        $productos = Product::where('nombre', 'like', '%' . $this->search . '%')
+            ->latest()->paginate();
+        return view('livewire.product-main', compact('productos'));
+    }
+
+    public function save()
+    {
+        $this->validate();
+        if (!$this->id) {
+            Product::create([
+                'nombre' => $this->nombre,
+                'descripcion' => $this->descripcion,
+                'cantidad' => $this->cantidad,
+                'precio' => $this->precio,
+                'disponible' => $this->disponible
+            ]);
+        } else {
+            $producto=Product::find($this->id);
+            $producto->update([
+                'nombre' => $this->nombre,
+                'descripcion' => $this->descripcion,
+                'cantidad' => $this->cantidad,
+                'precio' => $this->precio,
+                'disponible' => $this->disponible
+            ]);
+        }
+    }
+
+    public function edit(Product $item)
+    {
+        $this->id = $item->id;
+
+        $this->nombre = $item->nombre;
+        $this->descripcion = $item->descripcion;
+        $this->cantidad = $item->cantidad;
+        $this->precio = $item->precio;
+        $this->disponible = $item->disponible;
+        $this->modal('showform')->show();
+    }
+
+    public function create()
+    {
+        $this->reset(['id', 'nombre', 'descripcion', 'cantidad', 'precio', 'disponible']);
+        $this->modal('showform')->show();
     }
 }
--- SisVentasG2/app/Models/Product.php
+++ SisVentasG2/app/Models/Product.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     Use HasFactory;
+    protected $guarded=['id'];
 }
